407d81d4bd62a59681e02e27558551a2d21e6c42
jbjorgaard <jeremiah.bjorgaard@gmail.com>
2020-06-22 21:58:09 -0500

    combined viewLinks and viewData.  Replaced forEach loops with for of loops. cleaned up display methods to access viewData properly.

--- js/app.js
+++ js/app.js
@@ -1,42 +1,49 @@
 class Model {
   constructor() {
-    // state of the model, an array of navigation links that load views
-    this.viewLinks = [
-      { id: 0, linkName: 'Dashboard', linkHTML: '#dashboard', active:true},
-      { id: 1, linkName: 'Add New Issue', linkHTML: '#addIssue', active:false},
-      { id: 2, linkName: 'Search', linkHTML: '#search', active:false}
-    ];
+    // starting view data
     this.viewData = [
-      { id: 0, viewName: 'Dashboard', content: [
-        {id: 0, displayData: 'This is Some Dashboard Content.'}
-      ]}
+      {
+        id: 0, name: 'Dashboard', link: '#dashboard', active: true, content: [
+          'This is some dashboard content', 'This is some more dashboard content'
+        ]
+      },
+      {
+        id: 1, name: 'Add New Issue', link: '#addNewIssue', active: false, content: [
+          'This is the New Issue content', 'This is more New Issue content'
+        ]
+      },
+      {
+        id: 2, name: 'Search Issues', link: '#searchIssues', active: false, content: [
+          'This is our Search content', 'This is more Search content'
+        ]
+      }
     ];
 
   }
-
-  getActiveView(viewLinks) {
+  // Find which view is active and return an id
+  getActiveView(viewData) {
     var _id;
-    viewLinks.forEach(getLinks);
-    function getLinks(link) {
-      if(link.active === true) {
-        _id = link.id;
+    for (var item of viewData) {
+      if(item.active === true) {
+        _id = item.id;
       }
     }
+
     return _id;
   }
-
-  setActiveView(viewLinks, viewId) {
-    viewLinks.forEach(getLinks);
-    function getLinks(link) {
-      link.active = false;
+  // set all views to inactive and then set chosen view to active
+  setActiveView(viewData, id) {
+    for (var item of viewData) {
+      item.active = false;
     }
-    viewLinks[viewId].active = true;
-  }
 
+    viewData[id].active = true;
+  }
 }
 
 class View {
   constructor() {
+    // Create basic page structure
     this.app = this.getElement('#app');
     this.nav = this.createElement('nav');
     this.viewList = this.createElement('ul', 'viewList');
@@ -52,53 +59,52 @@ class View {
     this.content.append(this.contentList);
     this.app.append(this.nav, this.actions, this.content);
   }
-
+  // method to create new page elements
   createElement(tag, className) {
     const element = document.createElement(tag);
     if(className) element.classList.add(className);
 
     return element;
   }
-
+  // method to retrieve existing page elements
   getElement(selector) {
     const element = document.querySelector(selector);
 
     return element;
   }
-
-  displayViewLinks(viewLinks, viewList, createElement) {
-    viewLinks.forEach(getLinks);
-    function getLinks(link) {
-      const li = createElement('li');
-      li.id = link.id;
-      const a = createElement('a');
-      a.href = link.linkHTML;
-      a.append(link.linkName);
+  // populate nav menu with existing views
+  displayViewLinks(model, view) {
+    for (var item of model.viewData) {
+      const li = view.createElement('li');
+      li.id = item.id;
+      const a = view.createElement('a');
+      a.href = item.link;
+      a.append(item.name);
       li.append(a);
-      viewList.append(li);
+      view.viewList.append(li);
     }
   }
-  addContent(data, view) {
-    data.forEach(grabData);
-    function grabData(item) {
+  // add new content to page content
+  addContent(viewDataContent, view) {
+    for (var item of viewDataContent) {
       const li = view.createElement('li');
-      li.append(item.displayData);
+      li.append(item);
       view.contentList.append(li);
     }
   }
-
+  // returns an array of content from a view
   getData(id, viewData) {
     return viewData[id].content;
   }
-
+  // clears existing page content
   clearContent(contentList) {
     contentList.innerHTML = '';
   }
-
-  displayView(view, model) {
+  // calls clearContent and addContent methods
+  displayView(model, view) {
     view.clearContent(view.contentList);
 
-    view.addContent(view.getData(model.getActiveView(model.viewLinks), model.viewData), view);
+    view.addContent(view.getData(model.getActiveView(model.viewData), model.viewData), view);
   }
 
 
@@ -108,11 +114,13 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-
-    this.view.displayViewLinks(this.model.viewLinks, this.view.viewList, this.view.createElement);
-    this.view.displayView(this.view, this.model);
-
+  }
+  // seeds initial startup data and populates page
+  startup (model, view) {
+    this.view.displayViewLinks(this.model, this.view);
+    this.view.displayView(this.model, this.view);
   }
 }
 
 const app = new Controller(new Model(), new View());
+app.startup(app.model, app.view);
